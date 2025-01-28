c005a9e9d171dbb77a73ffef47a77e7c69ab1c5f
Marcelo MArsson <marsson@gmail.com>
2025-01-28 18:24:33 +1000

    Upgrading dependencies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // styles are not included in the single components
-import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css";
-
+import "@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
